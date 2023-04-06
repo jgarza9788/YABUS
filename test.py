0ef2dc2e5b090d61e60dddb2b0b4643d88ce48ed
jgarza9788 <jgarza9788@gmail.com>
2023-04-06 16:05:31 -0400

    made checks for test.py script

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import os,time
 import shutil
+import json5 as json
 from dataMan import DataManager
 
 from main import YABUS
@@ -26,7 +27,7 @@ def prepare_files():
     #copy two
     shutil.copy2('.\\test\\A\\Two.json','.\\test\\B\\A\\Two.json')
 
-    time.sleep(5)
+    time.sleep(1)
 
     #edit .\\test\\A\\Two.json
     Two.data.append('hello')
@@ -49,17 +50,46 @@ def run_yabus():
 
     config = DataManager('.\\test\\config.json',default=data)
 
-    yabus = YABUS(config_dir=config.file_dir,verbose=True,save_cache_as_csv=True)
+    yabus = YABUS(config_dir=config.file_dir,verbose=False,save_cache_as_csv=True)
     yabus.backup()
 
 def checks():
-    pass 
-    # todo
+
+    print('checking files')
+    
+    #these files should exists
+    filelist = [
+            '.\\test\\A\\One.json',
+            '.\\test\\A\\Two.json',
+            '.\\test\\B\\A\\One.json',
+            '.\\test\\B\\A\\Two.json',
+        ]
+
+    for f in filelist:
+        assert os.path.isfile(f), f' {f} should exists'
+
+    #count the number of files in .archive, should be 2
+    filelist = []
+    archive_dir = '.\\test\\B\\A\\.archive'
+    for dirpath, dirnames, filenames in os.walk(archive_dir):
+        for filename in filenames:
+            filelist.append(os.path.join(dirpath,filename))
+    
+    assert len(filelist) == 2, 'should archive 2 files'
+
+    # make sure the backup matches the original
+    with open('.\\test\\A\\Two.json','r') as fileA:
+        with open('.\\test\\B\\A\\Two.json','r') as fileB:
+            assert json.load(fileA) == json.load(fileB)
+
+    print('please see errors above, if any')
 
 if __name__ == '__main__':
     clean()
     prepare_files()
     run_yabus()
+    checks()
+    clean()
 
 
 # this is the result that is expected 
